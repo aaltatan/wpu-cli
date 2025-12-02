5da76a1e612e9c8252a98b7abc038b512d742c08
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2025-12-02 11:05:06 +0200

    rotate logger

--- cli/logger.py
+++ cli/logger.py
@@ -4,7 +4,8 @@ from loguru import logger
 def init_logger() -> None:
     logger.remove()
     logger.add(
-        "app.log",
+        "logs/app.log",
         level="INFO",
         format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {name}:{function}:{line} --> {message}",  # noqa: E501
+        rotation="2 days",
     )

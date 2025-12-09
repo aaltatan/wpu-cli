cb21f41d893da3f83bb952ba666a1ca5f559f539
ABDULLAH TATAN <WPU0001@corp.wpu.com>
2025-12-09 10:52:10 +0200

    implement raise_missing_option_exception function

--- cli/utils.py
+++ cli/utils.py
@@ -1,8 +1,33 @@
 from pathlib import Path
+from typing import Literal
 
+import click
 from playwright.sync_api import Page
 
-BASE_DIR = Path.cwd().parent.parent
+
+def raise_missing_option_exception(
+    param_type: Literal["option", "argument"],
+    param_name: str,
+    param_shortname: str | None = None,
+    message: str | None = None,
+) -> None:
+    if param_shortname is not None and param_type == "argument":
+        message = f"Cannot specify short name for argument '{param_name}'"
+        raise ValueError(message)
+
+    if param_type == "option":
+        param_name = f"--{param_name}"
+
+    param_hint = param_name
+
+    if param_shortname:
+        param_hint = f"-{param_shortname} / --{param_name}"
+
+    raise click.MissingParameter(
+        param_type=param_type,
+        param_hint=param_hint,
+        message=message,
+    )
 
 
 def get_authenticated_page(p, username: str, password: str) -> Page:

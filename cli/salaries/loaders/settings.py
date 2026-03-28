1b7685d513b4bb778c4b38e54f1ceef47a140ca1
aaltatan <a.altatan@gmail.com>
2026-03-28 17:10:58 +0300

    split class into functions

--- cli/salaries/loaders/settings.py
+++ cli/salaries/loaders/settings.py
@@ -15,93 +15,90 @@ class SettingNotInitializedError(Exception):
         super().__init__(message, *args)
 
 
-class SettingsLoader:
-    def __init__(self, book: xw.Book) -> None:
-        self._ws: xw.Sheet = book.sheets[SETTINGS_SHEET_NAME]
+def load_settings(wb: xw.Book) -> SettingsSchema:
+    frc = Decimal("0.01")
 
-    def load(self) -> SettingsSchema:
-        frc = Decimal("0.01")
+    ws = wb.sheets[SETTINGS_SHEET_NAME]
 
-        fixed_tax_rate = self._load_cell(SettingCell.FIXED_TAX_RATE).quantize(frc)
-        ss_deduction_rate = self._load_cell(SettingCell.SS_DEDUCTION_RATE).quantize(frc)
-        tu_monthly_deduction_rate = self._load_cell(SettingCell.TU_MONTHLY_DEDUCTION_RATE).quantize(
-            frc
-        )
-        tu_pension_deduction_rate = self._load_cell(SettingCell.TU_PENSION_DEDUCTION_RATE).quantize(
-            frc
-        )
-        healthy_leaves_rate = self._load_cell(SettingCell.HEALTHY_LEAVES_RATE).quantize(frc)
-        healthy_leaves_based_on = self._load_cell(SettingCell.HEALTHY_LEAVES_BASED_ON, str)
-        days_of_month = self._load_cell(SettingCell.DAYS_OF_MONTH).quantize(frc)
-        leaves_without_pay_rate = self._load_cell(SettingCell.LEAVES_WITHOUT_PAY_RATE)
-        leaves_without_pay_based_on = self._load_cell(SettingCell.LEAVES_WITHOUT_PAY_BASED_ON, str)
-        overtime_rate = self._load_cell(SettingCell.OVERTIME_RATE).quantize(frc)
-        overtime_based_on = self._load_cell(SettingCell.OVERTIME_BASED_ON, str)
-        additional_leaves_rate = self._load_cell(SettingCell.ADDITIONAL_LEAVES_RATE).quantize(frc)
-        additional_leaves_based_on = self._load_cell(SettingCell.ADDITIONAL_LEAVES_BASED_ON, str)
-        min_ss_salary = self._load_cell(SettingCell.MIN_SS_SALARY).quantize(frc)
+    fixed_tax_rate = _load_cell(ws, SettingCell.FIXED_TAX_RATE).quantize(frc)
+    ss_deduction_rate = _load_cell(ws, SettingCell.SS_DEDUCTION_RATE).quantize(frc)
+    tu_monthly_deduction_rate = _load_cell(ws, SettingCell.TU_MONTHLY_DEDUCTION_RATE).quantize(frc)
+    tu_pension_deduction_rate = _load_cell(ws, SettingCell.TU_PENSION_DEDUCTION_RATE).quantize(frc)
+    healthy_leaves_rate = _load_cell(ws, SettingCell.HEALTHY_LEAVES_RATE).quantize(frc)
+    healthy_leaves_based_on = _load_cell(ws, SettingCell.HEALTHY_LEAVES_BASED_ON, str)
+    days_of_month = _load_cell(ws, SettingCell.DAYS_OF_MONTH).quantize(frc)
+    leaves_without_pay_rate = _load_cell(ws, SettingCell.LEAVES_WITHOUT_PAY_RATE)
+    leaves_without_pay_based_on = _load_cell(ws, SettingCell.LEAVES_WITHOUT_PAY_BASED_ON, str)
+    overtime_rate = _load_cell(ws, SettingCell.OVERTIME_RATE).quantize(frc)
+    overtime_based_on = _load_cell(ws, SettingCell.OVERTIME_BASED_ON, str)
+    additional_leaves_rate = _load_cell(ws, SettingCell.ADDITIONAL_LEAVES_RATE).quantize(frc)
+    additional_leaves_based_on = _load_cell(ws, SettingCell.ADDITIONAL_LEAVES_BASED_ON, str)
+    min_ss_salary = _load_cell(ws, SettingCell.MIN_SS_SALARY).quantize(frc)
 
-        return SettingsSchema(
-            fixed_tax_rate=fixed_tax_rate,
-            ss_deduction_rate=ss_deduction_rate,
-            tu_monthly_deduction_rate=tu_monthly_deduction_rate,
-            tu_pension_deduction_rate=tu_pension_deduction_rate,
-            healthy_leaves_rate=healthy_leaves_rate,
-            healthy_leaves_based_on=healthy_leaves_based_on,  # type: ignore  # noqa: PGH003
-            days_of_month=days_of_month,
-            leaves_without_pay_rate=leaves_without_pay_rate,
-            leaves_without_pay_based_on=leaves_without_pay_based_on,  # type: ignore  # noqa: PGH003
-            overtime_rate=overtime_rate,
-            overtime_based_on=overtime_based_on,  # type: ignore  # noqa: PGH003
-            additional_leaves_rate=additional_leaves_rate,
-            additional_leaves_based_on=additional_leaves_based_on,  # type: ignore  # noqa: PGH003
-            fixed_tax_columns=self._load_fixed_tax_columns(),
-            brackets=self._load_brackets(),
-            min_ss_salary=min_ss_salary,
-        )
+    return SettingsSchema(
+        fixed_tax_rate=fixed_tax_rate,
+        ss_deduction_rate=ss_deduction_rate,
+        tu_monthly_deduction_rate=tu_monthly_deduction_rate,
+        tu_pension_deduction_rate=tu_pension_deduction_rate,
+        healthy_leaves_rate=healthy_leaves_rate,
+        healthy_leaves_based_on=healthy_leaves_based_on,  # type: ignore  # noqa: PGH003
+        days_of_month=days_of_month,
+        leaves_without_pay_rate=leaves_without_pay_rate,
+        leaves_without_pay_based_on=leaves_without_pay_based_on,  # type: ignore  # noqa: PGH003
+        overtime_rate=overtime_rate,
+        overtime_based_on=overtime_based_on,  # type: ignore  # noqa: PGH003
+        additional_leaves_rate=additional_leaves_rate,
+        additional_leaves_based_on=additional_leaves_based_on,  # type: ignore  # noqa: PGH003
+        fixed_tax_columns=_load_fixed_tax_columns(ws),
+        brackets=_load_brackets(ws),
+        min_ss_salary=min_ss_salary,
+    )
 
-    def _load_brackets(self) -> list[BracketSchema]:
-        rg = self._ws.range(BRACKETS_RANGE)
 
-        brackets: list[BracketSchema] = []
+def _load_brackets(ws: xw.Sheet) -> list[BracketSchema]:
+    rg = ws.range(BRACKETS_RANGE)
 
-        if not isinstance(rg.value, list):
-            message = f"Brackets range value is not a list: {rg.value}"
+    brackets: list[BracketSchema] = []
+
+    if not isinstance(rg.value, list):
+        message = f"Brackets range value is not a list: {rg.value}"
+        raise TypeError(message)
+
+    for row in rg.value:
+        _min, _max, rate = row
+
+        if not isinstance(_min, float):
+            message = f"Bracket min is not a float: {_min}"
             raise TypeError(message)
 
-        for row in rg.value:
-            _min, _max, rate = row
+        if not isinstance(_max, float):
+            message = f"Bracket max is not a float: {_max}"
+            raise TypeError(message)
 
-            if not isinstance(_min, float):
-                message = f"Bracket min is not a float: {_min}"
-                raise TypeError(message)
+        if not isinstance(rate, float):
+            message = f"Bracket rate is not a float: {rate}"
+            raise TypeError(message)
 
-            if not isinstance(_max, float):
-                message = f"Bracket max is not a float: {_max}"
-                raise TypeError(message)
-
-            if not isinstance(rate, float):
-                message = f"Bracket rate is not a float: {rate}"
-                raise TypeError(message)
-
-            brackets.append(
-                BracketSchema(
-                    min=Decimal(_min).quantize(Decimal(1)),
-                    max=Decimal(_max).quantize(Decimal(1)),
-                    rate=Decimal(rate).quantize(Decimal("0.01")),
-                )
+        brackets.append(
+            BracketSchema(
+                min=Decimal(_min).quantize(Decimal(1)),
+                max=Decimal(_max).quantize(Decimal(1)),
+                rate=Decimal(rate).quantize(Decimal("0.01")),
             )
+        )
 
-        return brackets
+    return brackets
 
-    def _load_fixed_tax_columns(self) -> list[str]:
-        rg = self._ws.range(FIXED_TAX_COLUMNS_RANGE)
-        return [str(cell.value) for cell in rg if cell.value is not None]
 
-    def _load_cell[T: (Decimal, str)](self, setting: SettingCell, cast: type[T] = Decimal) -> T:
-        value = self._ws.range(SETTINGS_CELLS_MAPPER[setting]).value
+def _load_fixed_tax_columns(ws: xw.Sheet) -> list[str]:
+    rg = ws.range(FIXED_TAX_COLUMNS_RANGE)
+    return [str(cell.value) for cell in rg if cell.value is not None]
 
-        if value is None:
-            raise SettingNotInitializedError(setting)
 
-        return cast(value)
+def _load_cell[T: (Decimal, str)](ws: xw.Sheet, setting: SettingCell, cast: type[T] = Decimal) -> T:
+    value = ws.range(SETTINGS_CELLS_MAPPER[setting]).value
+
+    if value is None:
+        raise SettingNotInitializedError(setting)
+
+    return cast(value)
